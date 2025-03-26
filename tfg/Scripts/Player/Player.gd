b8132d029b1a77b7ecb50f0cf212a3c58500def0
rmungar <mgraul@iesrafaelalberti.es>
2025-03-26 19:18:43 +0100

    Handle ground movement + camera tweaks

--- tfg/Scripts/Player/Player.gd
+++ tfg/Scripts/Player/Player.gd
@@ -1,31 +1,50 @@
 class_name Player extends CharacterBody2D
 
+@onready var SPRITE: Sprite2D = $Sprite2D
+@onready var ANIMATION_PLAYER: AnimationPlayer = $AnimationPlayer
 
 @export var SPEED: float = 200.0
-@export var JUMP_VELOCITY: float = -350.0
+@export var AIR_SPEED: float = -150.0
+@export var AIR_ACCELERATION: float = 0.1
+@export var JUMP_STRENGTH: float = -350.0
 @export var GRAVITY: float = 980.0
 @export var DELECERATION: float = 0.2
-
+var FACINGDIRECTION: float
 
 func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	# Handle jump
+	handleMovement(delta)
+	updateAnimations()
+	move_and_slide()
+
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+func handleMovement(delta: float) -> void:
 	# Handle directions
-	var facingDirection: float = 0.0
+	FACINGDIRECTION = 0.0
 	if Input.is_action_pressed("MoveLeft"):
-		facingDirection = -1.0
+		FACINGDIRECTION = -1.0	
 	elif Input.is_action_pressed("MoveRight"):
-		facingDirection = 1.0
-	
-	if facingDirection != 0:
-		velocity.x = facingDirection * SPEED
+		FACINGDIRECTION = 1.0
+	if FACINGDIRECTION != 0:
+		SPRITE.flip_h = FACINGDIRECTION < 0
+	if FACINGDIRECTION != 0:
+		velocity.x = FACINGDIRECTION * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * DELECERATION)
-
-	move_and_slide()
+	
+func updateAnimations() -> void:
+	if not is_on_floor():
+		if velocity.y < 0:
+			ANIMATION_PLAYER.play("Jump")
+		else:
+			ANIMATION_PLAYER.play("Fall")
+	else:
+		if velocity.x != 0:
+			ANIMATION_PLAYER.play("Run")
+		else:
+			ANIMATION_PLAYER.play("Idle")
