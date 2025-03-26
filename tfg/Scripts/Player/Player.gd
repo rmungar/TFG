02180bb68b04e7c9b59e6595d97d1cff4c55d8e0
rmungar <mgraul@iesrafaelalberti.es>
2025-03-26 21:49:59 +0100

    Check TileMapLayer's blurriness and fix it

--- tfg/Scripts/Player/Player.gd
+++ tfg/Scripts/Player/Player.gd
@@ -9,34 +9,51 @@ class_name Player extends CharacterBody2D
 @export var JUMP_STRENGTH: float = -350.0
 @export var GRAVITY: float = 980.0
 @export var DELECERATION: float = 0.2
-var FACINGDIRECTION: float
+var FACINGDIRECTION: float 
 
-func _physics_process(delta: float) -> void:
-	
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
 
-	handleMovement(delta)
-	updateAnimations()
-	move_and_slide()
 
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-func handleMovement(delta: float) -> void:
+func _process(delta: float) -> void:
 	# Handle directions
-	FACINGDIRECTION = 0.0
 	if Input.is_action_pressed("MoveLeft"):
 		FACINGDIRECTION = -1.0	
 	elif Input.is_action_pressed("MoveRight"):
 		FACINGDIRECTION = 1.0
+	
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		handleAirMovement(delta)
+	else:
+		handleGroundMovement(delta)
+	handleJump()
+	updateAnimations()
+	move_and_slide()
+
+	
+
+func handleGroundMovement(delta: float) -> void:
+
 	if FACINGDIRECTION != 0:
 		SPRITE.flip_h = FACINGDIRECTION < 0
-	if FACINGDIRECTION != 0:
+	if FACINGDIRECTION != 0 and (Input.is_action_pressed("MoveLeft") or Input.is_action_pressed("MoveRight")) :
 		velocity.x = FACINGDIRECTION * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * DELECERATION)
-	
+		velocity.x = 0.0
+
+func handleAirMovement(delta: float) -> void:
+	if FACINGDIRECTION:
+		SPRITE.flip_h = FACINGDIRECTION < 0
+		velocity.x = lerp(velocity.x, -FACINGDIRECTION * AIR_SPEED, AIR_ACCELERATION)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, AIR_ACCELERATION * 0.5)
+
+
+func handleJump() -> void:
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_STRENGTH
+
 func updateAnimations() -> void:
 	if not is_on_floor():
 		if velocity.y < 0:
