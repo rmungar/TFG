dc0a1cd551382caba2f62d38df9ec7038cfb76f0
rmungar <mgraul@iesrafaelalberti.es>
2025-03-31 21:20:57 +0200

    Enemy collision detection, gravity and start scene redesign

--- tfg/Scripts/Enemies/Enemy.gd
+++ tfg/Scripts/Enemies/Enemy.gd
@@ -13,3 +13,20 @@ class_name Enemy extends CharacterBody2D
 @export var CURRENT_HP = 3
 
 var FACING_DIRECTION: float = -1.0
+
+
+func _ready() -> void:
+	print("Enemy current health: ", CURRENT_HP)
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	move_and_slide()
+
+
+func _on_take_damage(amount: int) -> void:
+	CURRENT_HP -= amount
+	if CURRENT_HP <= 0:
+		queue_free()
