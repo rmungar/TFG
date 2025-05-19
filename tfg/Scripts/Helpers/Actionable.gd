4808db18e541802242b5702ae22633e19740ba50
rmungar <mgraul@iesrafaelalberti.es>
2025-05-19 20:26:17 +0200

    Make sure never to do this again

--- tfg/Scripts/Helpers/Actionable.gd
+++ tfg/Scripts/Helpers/Actionable.gd
@@ -1,10 +1,9 @@
 class_name Actionable extends Area2D
 
 
-@export var dialogueResource: DialogueResource
+
 @export var dialogueStart: String = "start"
 
 
 func action() -> void:
-	
-	DialogueManager.show_dialogue_balloon(dialogueResource, dialogueStart)
+	pass
